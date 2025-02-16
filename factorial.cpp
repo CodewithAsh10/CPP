2ce1c68ada81bdb894ac469a1b8261a5d7d06803
Arsh Bakshi <arsh.cp10@gmail.com>
2025-02-16 21:52:46 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int fact(int n) {
+    if (n==0 or n==1) {
+        return 1;
+    }
+    return n*fact(n-1);
+}
 int main() {
+// Factorial using Loops
     int n;
     cout<<"Enter the number: ";
     cin>>n;
@@ -11,5 +18,10 @@ int main() {
         fac = fac*i;
         i++;
     }
-    cout<<"Factorial of "<<n<<" : "<<fac;
+    cout<<"(Loops) Factorial of "<<n<<" : "<<fac<<endl;
+
+// Factorial using recursion
+    cout<<"(Recursion) Factorial of "<<n<<" : "<<fact(n);
+
 }
+
